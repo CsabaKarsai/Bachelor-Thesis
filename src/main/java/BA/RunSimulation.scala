d2c4d22c7994f967a2417a55aee1d6157bf9b22a
Csaba Karsai <csaba@karsai.info>
2020-05-25 11:49:00 +0200

    test in RunSimulation deleted

--- src/main/java/BA/RunSimulation.scala
+++ src/main/java/BA/RunSimulation.scala
@@ -5,9 +5,6 @@ import io.gatling.core.config.GatlingPropertiesBuilder
 
 object RunSimulation {
   def main(args: Array[String]) {
-
-    println("test5")
-
     val simClass = classOf[ThirdSimulation].getName
 
     val props = new GatlingPropertiesBuilder
