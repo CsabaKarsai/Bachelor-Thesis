aea759332cf236596feb9d3fcfff9a6d1bdbeed5
Csaba Karsai <csaba@karsai.info>
2020-06-07 21:26:54 +0200

    prettified RunSimulation

--- src/main/java/BA/RunMySimulation.scala
+++ src/main/java/BA/RunMySimulation.scala
@@ -5,8 +5,8 @@ import io.gatling.core.config.GatlingPropertiesBuilder
 
 object RunMySimulation {
   def main(args: Array[String]) {
-    val simClass = classOf[MySimulation].getName
 
+    val simClass = classOf[MySimulation].getName
     val props = new GatlingPropertiesBuilder
     props.simulationClass(simClass)
     Gatling.fromMap(props.build)
