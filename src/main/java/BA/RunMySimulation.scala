876c75fbd5377c459d910674e95da6cfce51f714
Csaba Karsai <csaba@karsai.info>
2020-06-23 22:25:53 +0200

    test xcomments for testing pushes removed. Project now imported PC with more CPU processing power.

--- src/main/java/BA/RunMySimulation.scala
+++ src/main/java/BA/RunMySimulation.scala
@@ -5,8 +5,6 @@ import io.gatling.core.config.GatlingPropertiesBuilder
 
 object RunMySimulation {
   def main(args: Array[String]) {
-    //test from laptop
-    //test from PC
 
     val simClass = classOf[MySimulation].getName
     val props = new GatlingPropertiesBuilder
