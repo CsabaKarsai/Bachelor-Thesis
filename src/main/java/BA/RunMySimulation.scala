7c09f24defea97a1089b35c96b1d2aeae83ed570
Csaba Karsai <csaba@karsai.info>
2020-06-23 22:22:30 +0200

    test push from PC

--- src/main/java/BA/RunMySimulation.scala
+++ src/main/java/BA/RunMySimulation.scala
@@ -5,7 +5,8 @@ import io.gatling.core.config.GatlingPropertiesBuilder
 
 object RunMySimulation {
   def main(args: Array[String]) {
-    //test
+    //test from laptop
+    //test from PC
 
     val simClass = classOf[MySimulation].getName
     val props = new GatlingPropertiesBuilder
